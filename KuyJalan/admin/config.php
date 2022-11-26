7f4421963bbd20eaaa76c6cff383a11a02a9ab69
Akbar Abdurrahman Jafaruddin <96691090+AkbarAj@users.noreply.github.com>
2022-11-26 09:41:42 +0800

    Add files via upload

---
+++ KuyJalan/admin/config.php
@@ -0,0 +1,7 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'sight_in_japan';
+$databaseUsername = 'root';
+$databasePassword = '';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+?>
